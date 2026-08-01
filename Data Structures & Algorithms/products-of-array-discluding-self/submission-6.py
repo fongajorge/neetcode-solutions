ce61d529890f8cddb682e1a4ff19fe8e2ccd15fe
Jorge Fong <fongajorge@gmail.com>
2026-08-01 13:54:34 -0600

    Add: products-of-array-discluding-self - submission-6

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-6.py
@@ -0,0 +1,15 @@
+# Brute force solution, O(n^2)
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        answer = []
+
+        for i in range(len(nums)):
+            mult = 1
+
+            for j in range(len(nums)):
+                if i != j:
+                    mult *= nums[j]
+            
+            answer.append(mult)
+
+        return answer
