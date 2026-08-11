d964936763ecb4ff303c89b0ab8f25528385e816
Jorge Fong <fongajorge@gmail.com>
2026-08-11 11:38:43 -0600

    Add: eating-bananas - submission-9

---
+++ Data Structures & Algorithms/eating-bananas/submission-9.py
@@ -0,0 +1,25 @@
+# Binary Search
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l = 1
+        r = max(piles) + 1
+
+        currentMin = max(piles) + 2
+
+        while l <= r:
+            m = (l + r) // 2
+            
+            hours = 0
+
+            for element in piles:
+                hours += -(-element // m)
+
+            if hours <= h:
+                if m < currentMin:
+                    currentMin = m
+                
+                r = m - 1
+            else:
+                l = m + 1
+
+        return currentMin
